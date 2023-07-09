7c8059e9a2ae358a74abdc0115ba0468fe6c9d78
leandjb <leandjb@gmail.com>
2023-07-09 01:23:00 -0500

    Refector for login activity

--- app/src/main/java/com/kotlinclientapp/MainActivity.kt
+++ app/src/main/java/com/kotlinclientapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.kotlinclientapp
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
 
     }
